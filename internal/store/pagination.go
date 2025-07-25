02a78d2956a110c7a836a87f739283bf91409d07
Uday Teja <dev@udayteja.com>
2025-07-26 02:46:08 +0530

    add codes

--- internal/store/pagination.go
+++ internal/store/pagination.go
@@ -9,9 +9,9 @@ import (
 )
 
 type PaginatedFeedQuery struct {
-	Limit  int      `json:"limit"  validate:"gte=1,lte=20"`   // Enforces 1 ≤ Limit ≤ 20
-	Offset int      `json:"offset" validate:"gte=0"`          // Must be ≥ 0
-	Sort   string   `json:"sort"   validate:"oneof=asc desc"` // Only "asc" or "des" allowed
+	Limit  int      `json:"limit"  validate:"gte=1,lte=20"`
+	Offset int      `json:"offset" validate:"gte=0"`
+	Sort   string   `json:"sort"   validate:"oneof=asc desc"`
 	Tags   []string `json:"tags" validate:"max=5"`
 	Search string   `json:"search" validate:"max=100"`
 	Since  string   `json:"since"`
